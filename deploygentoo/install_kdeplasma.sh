90b6c9c62aab27124b31f32c29d663205b33a431
Vitalth137 <vitalth137@gmail.com>
2025-02-06 10:17:08 +0330

    install_kdeplasma.sh (It might bloat the system but recommended for better stability)

--- deploygentoo/install_kdeplasma.sh
+++ deploygentoo/install_kdeplasma.sh
@@ -2,9 +2,6 @@ DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' kdeplasma.txt | tr '\n' ' '`"
 emerge -vaN --autounmask $DEPLIST
 env-update
 source /etc/profile
-updatedb
-emerge --config sys-kernel/gentoo-kernel
-grub-mkconfig -o /boot/grub/grub.cfg
 
 echo [OK] KDE Plasma (FUll) installed!
 echo It's recommended to REBOOT the system.
