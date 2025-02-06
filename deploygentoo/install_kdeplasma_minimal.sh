f5059ac8522b36a80f6e88d51959f1f117112c8e
Vitalth137 <vitalth137@gmail.com>
2025-02-06 10:16:14 +0330

    install_kdeplasma_minimal.sh

--- deploygentoo/install_kdeplasma_minimal.sh
+++ deploygentoo/install_kdeplasma_minimal.sh
@@ -1,2 +1,7 @@
 DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' kdeplasma-minimal.txt | tr '\n' ' '`"
 emerge -avuN --autounmask $DEPLIST
+env-update
+source /etc/profile
+
+echo [OK] KDE Plasma (MINIMAL) installed!
+echo It's recommended to REBOOT the system.
